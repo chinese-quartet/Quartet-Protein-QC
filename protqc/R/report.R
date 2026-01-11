727b4f5a46abfb510e6f2f74ad92fce56e853d68
markx945 <632115230@qq.com>
2026-01-11 13:25:00 +0800

    change report format

--- protqc/R/report.R
+++ protqc/R/report.R
@@ -70,24 +70,24 @@ generate_protein_report <- function(qc_result,
   # --- 1. 定义中文文本内容 (基于 ShenKang-Quartet-Protein-Report_v0.2.docx) ---
   
   # 摘要
-  text_abstract <- "本报告基于多项组学关键质量控制指标，总结了 Quartet Protein参考物质所生成数据的质量情况。质量控制流程从用户输入肽段表达矩阵开始，分别计算每批次的检测特征数（Number of features）、标称特性召回率 (Recall of nominal characteristics)、信噪比 (Signal-to-Noise Ratio, SNR)、与参考数据集的相对相关性 (Relative Correlation with Reference Datasets, RC)及整体质量判断。"
+  text_abstract <- "本报告基于多项组学关键质量控制指标，总结了 Quartet Protein参考物质所生成数据的质量情况。质量控制流程从用户输入肽段表达矩阵开始，分别计算外部质控品的检测特征数（Number of features）、标称特性灵敏度 (Recall of nominal characteristics)、信噪比 (Signal-to-Noise Ratio, SNR)、与参考数据集的Pearson相关系数（Pearson correlation coefficient, PCC）及整体质量判断。"
   
   # 指标定义
   def_features_title <- "检测特征数（Number of features）"
   def_features_desc  <- "该指标反映在蛋白组检测中成功鉴定并映射到基因符号的蛋白数量。通常期望获得尽可能多的蛋白特征，以支持后续的生物学分析。"
   
-  def_recall_title   <- "标称特性召回率（Recall of nominal characteristics）"
+  def_recall_title   <- "标称特性灵敏度（Recall of nominal characteristics）"
   def_recall_desc    <- "定义为标称特性肽段在测试数据集中被成功检测到的比例。"
   
   def_snr_title      <- "信噪比（Signal-to-Noise Ratio, SNR）"
   def_snr_desc       <- "用于刻画某一检测平台、实验室或批次区分不同生物样本组之间内在生物学差异（“信号”）与同一样本组技术重复变异（“噪声”）的能力。SNR 越高，表明区分多组样本差异的能力越强。"
   
-  def_rc_title       <- "与参考数据集的相对相关性（Relative Correlation with Reference Datasets, RC）"
+  def_rc_title       <- "与参考数据集的Pearson相关系数（Pearson correlation coefficient, PCC）"
   def_rc_desc        <- "用于评估测试数据在相对定量层面与参考数据集之间的一致性。在 shotgun 蛋白组学中，肽段层面的定量在理论上更为可靠。因此，参考数据集基于历史数据中各样本对 (D5/D6、F7/D6) 在肽段水平的相对表达值（log2FC）构建。在评估过程中，仅选取在测试数据与参考数据集中均覆盖、且满足统计学阈值（p < 0.05）的肽段 log2FC 作为输入，计算测试数据与参考数据之间的 Pearson 相关系数，作为 RC 指标。"
   
   # 参考文献
   text_refs <- c(
-    "1. Zheng Y, et al. Multi-omics data integration using ratio-based quantitative profiling with Quartet reference materials. Nature Biotechnology, 2024.",
+    "1. Zheng Y, et al. Multi-omics data integration using ratio-based quantitative profiling with Quartet reference materials. Nature Biotechnology, 2024",
     "2. Tian S, et al. Quartet protein reference materials and datasets for multi-platform assessment of label-free proteomics. Genome Biology, 2023.",
     "3. 上海临床队列组学检测工作指引（征求意见稿）, 2025/11/26"
   )
@@ -123,7 +123,7 @@ generate_protein_report <- function(qc_result,
   val_snr    <- get_val(raw_table, "Signal-to-Noise Ratio")
   val_rc     <- get_val(raw_table, "Relative Correlation")
   
-  batch_name <- "QC_test" # 可改为从参数传入
+  batch_name <- "Query_data" # 可改为从参数传入
   
   # 格式化与阈值判断
   # 1. Features
@@ -153,9 +153,16 @@ generate_protein_report <- function(qc_result,
     if (val_rc < 0.80) str_rc <- paste0(str_rc, " ↓")
   }
   
-  # 整体判断 (Pass = Recall>=0.90 & SNR>=10 & RC>=0.80)
-  if (!is.na(val_recall) && !is.na(val_snr) && !is.na(val_rc)) {
-    pass <- (val_recall >= 0.90) && (val_snr >= 10) && (val_rc >= 0.80)
+  # # 整体判断 (Pass = Recall>=0.90 & SNR>=10 & RC>=0.80)
+  # if (!is.na(val_recall) && !is.na(val_snr) && !is.na(val_rc)) {
+  #   pass <- (val_recall >= 0.90) && (val_snr >= 10) && (val_rc >= 0.80)
+  #   str_qual <- ifelse(pass, "Pass", "No")
+  # } else {
+  #   str_qual <- "No" # 数据缺失默认为 No
+  # }
+  
+  if (!is.na(val_recall) && !is.na(val_snr)) {
+    pass <- (val_recall >= 0.90) && (val_snr >= 10)
     str_qual <- ifelse(pass, "Pass", "No")
   } else {
     str_qual <- "No" # 数据缺失默认为 No
@@ -163,11 +170,11 @@ generate_protein_report <- function(qc_result,
   
   # 构建 2行 x 6列 数据框
   df_table <- data.frame(
-    "批次" = c("推荐质量标准", batch_name),
+    "样本组" = c("推荐质量标准", batch_name),
     "检测特征数" = c("-", str_feat),
-    "标称特性召回率" = c("≥0.90", str_recall),
+    "标称特性灵敏度" = c("≥0.90", str_recall),
     "信噪比" = c("≥10", str_snr),
-    "相对相关性" = c("≥0.80", str_rc),
+    "Pearson相关系数" = c("≥0.80", str_rc),
     "整体质量" = c("全部通过", str_qual),
     check.names = FALSE
   )
@@ -175,6 +182,7 @@ generate_protein_report <- function(qc_result,
   # 创建 Flextable
   ft1 <- flextable(df_table) %>%
     theme_box() %>%
+    flextable::font(part = "all", fontname = "Times New Roman") %>%
     align(align = "center", part = "all") %>%
     width(width = 1.1) %>%  # 适当调整列宽
     bold(part = "header") %>%
@@ -221,24 +229,25 @@ generate_protein_report <- function(qc_result,
     # body_add_gg(value = qc_result$results$cor_results$cor_plot, style = "centered") %>%
     # body_add_break() %>%
     
-    # 6. 参考文献
+    # 5. 参考文献
     body_add_par(value = "参考文献", style = "heading 2") %>%
     body_add_par(value = text_refs[1], style = "Normal") %>%
     body_add_par(value = text_refs[2], style = "Normal") %>%
     body_add_par(value = text_refs[3], style = "Normal") %>%
     body_add_par(value = " ", style = "Normal") %>%
     
-    # 7. 免责声明
+    # 6. 免责声明
     body_add_par(value = "免责声明", style = "heading 3") %>%
     body_add_par(value = text_disclaimer, style = "Normal") %>%
+    body_add_break() %>%
   
-    # 5. 图片 (SNR 和 Correlation)
+    # 7. 图片 (SNR 和 Correlation)
     # 确保 qc_result 中有这些 plot 对象
     body_add_par(value = "Signal-to-Noise Ratio", style = "heading 2") %>%
     body_add_gg(value = qc_result$results$snr_results$snr_plot, style = "centered") %>%
     body_add_break() %>%
     
-    body_add_par(value = "Correlation with Reference Datasets", style = "heading 2") %>%
+    body_add_par(value = "Pearson Correlation Coefficient", style = "heading 2") %>%
     body_add_gg(value = qc_result$results$cor_results$cor_plot, style = "centered") %>%
   
   # 输出文件
