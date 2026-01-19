69f7a2ab7d812e172ac46eb39a05b36812a5e4f6
markx945 <632115230@qq.com>
2026-01-19 16:35:15 +0800

    adjust format

--- protqc/test/test.R
+++ protqc/test/test.R
@@ -8,13 +8,10 @@ library(protqc)
 example_prot_data_path <- system.file("extdata", "proteomics_pipeline_data_template.csv", package = "protqc")
 example_prot_metadata_path <- system.file("extdata", "proteomics_pipeline_meta_template.csv", package = "protqc")
 
-
-
 ## Calculate QC metrics
 prot_result <- protqc::qc_conclusion(example_prot_data_path, example_prot_metadata_path)
 
 prot_result
-
 
 ## Generate report
 report_template <- system.file("extdata", "quartet_template.docx", package = "protqc")
@@ -22,10 +19,6 @@ report_template
 
 prot_result$conclusion
 
-
 generate_protein_report(qc_result=prot_result, report_template = report_template,report_dir = './test/')
 
-
 data("reference_dataset", package = "protqc")
-
-
